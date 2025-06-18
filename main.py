73287c9a7eab19917cd01be060578df1fd15117d
JESHWANTH-009 <jeshwanth912@gmail.com>
2025-06-18 11:30:58 +0530

    updated

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ api_key = "0ca0ce081605356f5e30738979ba0355"
 url= f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
 
 response=requests.get(url)
-if response.status_codes==200:
+if response.status_code==200:
     data=response.json()
-    print("weather temp :",data)
+    print("weather type :",data['weather'][0]['description'])
+    print("temparature",data['main']['temp'])
